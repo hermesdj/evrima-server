0ee76271f73ea73ae473549bfdbad02f26d7e39f
dallardj@gmail.com <dallardj@gmail.com>
2023-05-04 17:41:52 +0200

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -4,10 +4,12 @@ mkdir -p "${STEAMAPPDIR}" || true
 # Override SteamCMD launch arguments if necessary
 # Used for subscribing to betas or for testing
 if [ -z "$STEAMCMD_UPDATE_ARGS" ]; then
-	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "$STEAMAPPDIR" +login anonymous +app_update "$STEAMAPPID" +quit
+	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "$STEAMAPPDIR" +login anonymous +app_update "$STEAMAPPID" -beta evrima +quit
 else
 	steamcmd_update_args=($STEAMCMD_UPDATE_ARGS)
-	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "$STEAMAPPDIR" +login anonymous +app_update "$STEAMAPPID" "${steamcmd_update_args[@]}" +quit
+	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "$STEAMAPPDIR" +login anonymous +app_update "$STEAMAPPID" -beta evrima "${steamcmd_update_args[@]}" +quit
 fi
 
 cd "${STEAMAPPDIR}"
+
+ls
